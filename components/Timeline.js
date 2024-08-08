83d74f1af1115786282ea78bfdfc4a90231d4f17
Ashwin0006 <ashcrafts0006@gmail.com>
2024-08-08 14:55:36 +0530

    Changes in id

--- components/Timeline.js
+++ components/Timeline.js
@@ -3,7 +3,7 @@ import AnimateDate from "./Animations/AnimateDate";
 
 export default function Timeline({ events }) {
   return events.map(({ name, description, start_date }) => (
-    <div className="relative border-l border-black dark:border-white/50 pl-8 pb-4 max-w-2xl ml-24 xl:ml-0">
+    <div key={name + start_date} className="relative border-l border-black dark:border-white/50 pl-8 pb-4 max-w-2xl ml-24 xl:ml-0">
       <div className="pb-12">
         <AnimateDate date={start_date} />
         <div className="w-4 h-4 border rounded-full absolute -top-1 -left-2 z-10 bg-black dark:bg-white" />
