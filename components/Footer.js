5ae255a1029298fa9ddbe5c23014e21dc2de4f1a
Ashwin0006 <ashcrafts0006@gmail.com>
2024-08-11 21:40:00 +0530

    Changes in Copyright

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <div className="bg-gray-200 dark:bg-gray-800 text-gray-600 font-semibold dark:text-gray-400 w-full flex flex-col justify-center items-center py-7">
         <h4 className="lg:text-[1.1rem]">
-          Copyright © 2022 Procode, Dept of IT, SSN
+          Copyright © 2024 Procode, Dept of IT, SSN
         </h4>
         <div className="mt-2 flex justify-end items-center gap-4">
           <a
